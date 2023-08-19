be4e150d1ce32e33660a9c9642ac9ea92fcd0006
Daemon0ps <133270668+Daemon0ps@users.noreply.github.com>
2023-08-19 13:06:47 -0700

    Create colourmod.py
    colours taste like aleph null.

---
+++ colourmod.py
@@ -0,0 +1,100 @@
+from termcolor import cprint
+
+def cp_lf()->str: 
+    return  cprint("", "black") 
+
+def cp_bk(x)->str: 
+    return  cprint(x, "black",end="") 
+
+def cp_r(x)->str: 
+    return  cprint(x, "red",end="") 
+
+def cp_g(x)->str: 
+    return  cprint(x, "green",end="")
+
+def cp_y(x)->str: 
+    return  cprint(x, "yellow",end="")
+    
+def cp_b(x)->str: 
+    return  cprint(x, "blue",end="")
+
+def cp_m(x)->str: 
+    return  cprint(x, "magenta",end="")
+
+def cp_c(x)->str: 
+    return  cprint(x, "cyan",end="")
+
+def cp_w(x)->str: 
+    return  cprint(x, "white",end="")
+
+def cp_lgr(x)->str: 
+    return  cprint(x, "light_grey",end="")
+
+def cp_dg(x)->str: 
+    return  cprint(x, "dark_grey",end="")
+
+def cp_lr(x)->str: 
+    return  cprint(x, "light_red",end="")
+
+def cp_lg(x)->str: 
+    return  cprint(x, "light_green",end="")
+
+def cp_ly(x)->str: 
+    return  cprint(x, "light_yellow",end="")
+
+def cp_lb(x)->str: 
+    return  cprint(x, "light_blue",end="")
+
+def cp_lm(x)->str: 
+    return  cprint(x, "light_magenta",end="")
+
+def cp_lc(x)->str: 
+    return  cprint(x, "light_cyan",end="")
+
+def cp_obk(x)->str: 
+    return  cprint(x, "on_black",end="")
+
+def cp_or(x)->str: 
+    return  cprint(x, "on_red",end="")
+
+def cp_og(x)->str: 
+    return  cprint(x, "on_green",end="")
+
+def cp_oy(x)->str: 
+    return  cprint(x, "on_yellow",end="")
+
+def cp_ob(x)->str: 
+    return  cprint(x, "on_blue",end="")
+    
+def cp_om(x)->str: 
+    return  cprint(x, "on_magenta",end="")
+    
+def cp_oc(x)->str: 
+    return  cprint(x, "on_cyan",end="")
+    
+def cp_ow(x)->str: 
+    return  cprint(x, "on_white",end="")
+    
+def cp_olgr(x)->str: 
+    return  cprint(x, "on_light_grey",end="")
+    
+def cp_odg(x)->str: 
+    return  cprint(x, "on_dark_grey",end="")
+    
+def cp_olr(x)->str: 
+    return  cprint(x, "on_light_red",end="")
+    
+def cp_olg(x)->str: 
+    return  cprint(x, "on_light_green",end="")
+    
+def cp_oly(x)->str: 
+    return  cprint(x, "on_light_yellow",end="")
+    
+def cp_olb(x)->str: 
+    return  cprint(x, "on_light_blue",end="")
+    
+def cp_olm(x)->str:
+    return  cprint(x, "on_light_magenta",end="")
+    
+def cp_olc(x)->str: 
+    return  cprint(x, "on_light_cyan",end="")
